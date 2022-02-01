62a13de6d65253652d9ccf06bd79ef1060b89552
maciek <macieq2048@gmail.com>
2022-02-01 18:27:40 +0100

    handle api errors

--- src/utils/apiHelpers.ts
+++ src/utils/apiHelpers.ts
@@ -4,15 +4,19 @@ const BASE_URL = "https://recruitment01.vercel.app/api";
 const INIT_PROJECT_URL = `${BASE_URL}/init`
 const PROJECT_URL = (id: string) => `${BASE_URL}/project/${id}`
 
-export const fetchProjectDetails = async (id: string = ""): Promise<ProjectRootType> => {
+export const fetchProjectDetails = async (id: string = ""): Promise<{ok: boolean, project: ProjectRootType}> => {
   if(!id){
-    id = (await fetchInitProject()).id;
+    const {respOk, project} = (await fetchInitProject());
+    if(respOk){
+      id = project.id;
+    }
   }
   const response = await fetch(PROJECT_URL(id))
-  return await response.json();
+
+  return {ok: response.ok, project: response.ok ? (await response.json()): {}};
 }
 
-export const fetchInitProject = async(): Promise<ProjectInitType> => {
+export const fetchInitProject = async(): Promise<{ok: boolean, project: ProjectInitType}> => {
   const response = await fetch(INIT_PROJECT_URL)
-    return await response.json();
+  return {ok: response.ok, project: response.ok ? (await response.json()): {}};
 }
