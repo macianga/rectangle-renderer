413f9f6343d35b8da6c9bcf0680e2a8a92253ab8
maciek <macieq2048@gmail.com>
2022-02-01 14:47:24 +0100

    add skeleton of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+  const [projectIdInput, setProjectIdInput] = useState("")
+
+  const buttonClicked = () => {
+    console.log("asd");
+  }
 
   return (
-    <div>
-        asd
+    <div className="main-content">
+      <div className="inputs">
+        <span>
+          Project ID:
+        </span>
+        <input
+          type="text"
+          value={projectIdInput}
+          onChange={e => setProjectIdInput(e.target.value)}
+          placeholder="For random leave empty :)"
+        />
+        <button
+          onClick={buttonClicked}
+        >Fetch
+        </button>
+      </div>
+      <svg height="100%" width="100%">
+      </svg>
     </div>
   )
 }
