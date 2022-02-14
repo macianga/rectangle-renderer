d0c4a76de7f29d95fc3b946ac973517d06d4e0e8
maciek <macieq2048@gmail.com>
2022-02-14 12:09:13 +0100

    remove unnecessary variable

--- src/utils/validateProjectData.ts
+++ src/utils/validateProjectData.ts
@@ -8,8 +8,7 @@ export const validateProjectData = (data: ProjectRootType): boolean => {
   if (!project.name) return false;
   if (project.width <= 0 || project.height <= 0) return false
   if (!project.items?.length) return false;
-  const items = project.items;
-  return items.every((rect) => {
+  return project.items.every((rect) => {
     return rect.id && rect.height >= 0 && rect.width >= 0 && rect.color && rect.rotation > 0;
   })
 }
